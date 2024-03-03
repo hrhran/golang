53fc3a5c112be79e6f9a6d925d6dba73c495fb2f
Hariharan S <hariharan0044@gmail.com>
2024-03-03 09:15:49 +0530

    leet_2652

---
+++ leet_2652.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func sumOfMultiples(n int) int {
+    sum := 0
+    for i := 1; i <= n; i++ {
+        if i % 3 == 0 || i % 5 == 0 || i % 7 == 0 {
+            sum += i
+        }
+    }
+    return sum
+}
+
+func main() {
+	n := 7
+	fmt.Println(sumOfMultiples(n))
+	n = 20
+	fmt.Println(sumOfMultiples(n))
+}
